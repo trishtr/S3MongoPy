62123f0502ece47dfdf69f715411cc7909224fec
Trang Tr <tran.trang@healthstream.com>
2023-02-17 12:32:16 -0900

    unitIdMap

--- testBase/QAEnv/unitIdDomainIdMap.py
+++ testBase/QAEnv/unitIdDomainIdMap.py
@@ -6,21 +6,24 @@ from testBase.QAEnv.filterMGCollection import MGFilter
 class unitDomainMap:
     filter = MGFilter()
     docs = filter.unitTaxonomyFilterTestClient()
+    unitIdMap = {}
 
     def unit_domainId_map(self):
 
         for doc in self.docs:
 
             id = doc.get('_id')
-            # print(id)
-            # print(type(id))
 
             for internalId in doc.get('internalIds'):
                 domainId = internalId.get('domainId')
-                # print(domainId)
-                unit_domainId_map = {}
-                unit_domainId_map[domainId] = id
-            print(unit_domainId_map)
+
+            self.unitIdMap[domainId] = id
+        # print(self.unitIdMap)
+        return self.unitIdMap
+
+
+
+
 
 
 
